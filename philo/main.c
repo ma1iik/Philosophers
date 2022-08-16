ebac2f1ab1ae22973d49ab6b340754574431647c
Malik Israilov <misrailo@c3r2p2.42nice.fr>
2022-08-16 18:24:24 +0200

    bonus

---
+++ philo/main.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misrailo <misrailo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 14:28:03 by misrailo          #+#    #+#             */
+/*   Updated: 2022/07/17 01:01:39 by misrailo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+void	init_forks(char **av, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < ft_atoi(av[1]))
+	{
+		if (i == 0)
+			data[i].right_fork = &data[ft_atoi(av[1]) - 1].left_fork;
+		else
+			data[i].right_fork = &data[i - 1].left_fork;
+		i++;
+	}
+}
+
+void	phils_data(int ac, char **av, t_data *data)
+{
+	int		i;
+
+	i = 0;
+	pthread_mutex_init(&data->output, NULL);
+	while (i < ft_atoi(av[1]))
+	{
+		data[i].av1 = ft_atoi(av[1]);
+		data[i].phil_id = i + 1;
+		data[i].death_time = ft_atoi(av[2]);
+		data[i].eat_time = ft_atoi(av[3]);
+		data[i].sleep_time = ft_atoi(av[4]);
+		data[i].starving = 0;
+		data[i].birth = 0;
+		data[i].right_fork = 0;
+		pthread_mutex_init(&data[i].left_fork, NULL);
+		if (ac == 6)
+			data[i].eat_nbr = ft_atoi(av[5]);
+		else
+			data[i].eat_nbr = MAX;
+		i++;
+	}
+	init_forks(av, data);
+}
+
+void	ft_free(t_data *data)
+{
+	int		i;
+
+	i = 0;
+	pthread_mutex_destroy(&data->output);
+	while (i < data->av1)
+	{
+		pthread_mutex_destroy(&data[i].left_fork);
+		i++;
+	}
+	free(data);
+}
+
+int	main(int ac, char **av)
+{
+	t_data		*data;
+
+	data = ft_calloc(sizeof(t_data), ft_atoi(av[1]));
+	if (ac != 5 && ac != 6)
+		return (0);
+	if (parsing(ac, av))
+		return (0);
+	phils_data(ac, av, data);
+	phils_thread(av, data);
+	ft_free(data);
+	return (0);
+}
---
+++ philo/parsing.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misrailo <misrailo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 19:57:13 by misrailo          #+#    #+#             */
+/*   Updated: 2022/07/17 01:01:05 by misrailo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (0);
+	else if (c == '+' || c == '-')
+		return (0);
+	else
+		return (1);
+}
+
+int	check_if_digit(int ac, char **av)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			if (ft_isdigit(av[i][j]))
+			{
+				printf("Not number\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	parsing(int ac, char **av)
+{
+	int		i;
+
+	i = 0;
+	while (av[++i])
+	{
+		if (ft_atoi(av[i]) < 0 || ft_atoi(av[i]) >= MAX)
+			return (1);
+	}
+	if (check_if_digit(ac, av))
+		return (1);
+	return (0);
+}
+
+void	*ft_memset(void *s, int c, size_t len)
+{
+	unsigned char	*dest;
+	size_t			i;
+
+	dest = s;
+	i = 0;
+	while (i < len)
+	{
+		*dest++ = c;
+		i++;
+	}
+	return (s);
+}
